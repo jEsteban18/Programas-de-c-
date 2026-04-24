fd48b9ea3a8e9b90e4b13f9438a926db612abf5d
jEsteban18 <126209836+jEsteban18@users.noreply.github.com>
2026-04-23 21:46:20 -0500

    Create Revisar_en_casa.cpp

---
+++ Revisar_en_casa.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <time.h>
+/* HACER UN MENU OPCION 1 GUAR DATOS LLENAR ARREGLOS 
+OPCION 2 MOSTRAR 
+OPCION 3 SALIR 
+SOLO SE PUEDE MOSTRAR DATOS SI SE HAN LENADOS */
+using namespace std;
+#define team 3
+int main() {
+	int num1 = 0, num2 = 0, num3 =0, opc;
+	int arreglo [team];
+	bool bandera = false;	
+	
+do {
+	cout << "1 digitar numeros\n 2: mostrar numeros\n 3: salir \n ";
+	cin >> opc;
+	switch (opc){
+		
+case 1: cout << "digita 3 numeros ";
+
+	break;
+		case 2: 
+
+	}
+	else {
+;
+break;
+	}
+	}
+
+}
+}
+
+	default: cout <<"opcion no valida";
+	break;
+	
+		
+	return 0;
+}
