9ad7238fce5bae0570f1d541a029881a47d660fb
jEsteban18 <126209836+jEsteban18@users.noreply.github.com>
2026-04-27 15:22:29 -0500

    MENU_CON_ARREGLOS_and_BANDERAS.cpp

--- Revisar_en_casa.cpp
+++ Revisar_en_casa.cpp
@@ -1,39 +1,52 @@
-#include <iostream>
-#include <time.h>
 /* HACER UN MENU OPCION 1 GUAR DATOS LLENAR ARREGLOS 
 OPCION 2 MOSTRAR 
 OPCION 3 SALIR 
 SOLO SE PUEDE MOSTRAR DATOS SI SE HAN LENADOS */
+#include <iostream>
+#include <time.h>
+
 using namespace std;
 #define team 3
 int main() {
-	int num1 = 0, num2 = 0, num3 =0, opc;
+	int opc, num = 0;
 	int arreglo [team];
 	bool bandera = false;	
 	
-do {
-	cout << "1 digitar numeros\n 2: mostrar numeros\n 3: salir \n ";
-	cin >> opc;
-	switch (opc){
-		
-case 1: cout << "digita 3 numeros ";
-
-	break;
+	do {
+		cout << "1 digitar numeros\n 2: mostrar numeros\n 3: salir \n ";
+		cin >> opc;
+		switch (opc){
+			
+		case 1: 
+			cout << "digita 3 numeros\n ";
+			for (int x =0;x < team;x++){
+			
+			cin >> num;
+			arreglo[x] = num;
+		}
+		bandera = true;
+			cout << "...guardando...\n";
+		break;
 		case 2: 
-
-	}
-	else {
-;
-break;
-	}
-	}
-
-}
-}
-
-	default: cout <<"opcion no valida";
-	break;
-	
+				
+			if (bandera == false){
+				cout << "digite los numeros\n";
+			}
+			else {
+			 for (int x =0;x<3;x++){	
+				cout << arreglo[x]<< "\n";
+			}
+			}
 		
-	return 0;
+		break;
+			
+	case 3 : cout << "saliendo ";
+	break;
+		default: cout << "opcion no valida\n";
+
+	}
+		
+	}
+	while (opc !=3);
+return 0;
 }
