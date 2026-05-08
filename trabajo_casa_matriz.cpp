a5b3fbe185bb0e60384644273df1ea86c786c476
jEsteban18 <126209836+jEsteban18@users.noreply.github.com>
2026-05-07 21:24:37 -0500

    Create trabajo_casa_matriz.cpp

---
+++ trabajo_casa_matriz.cpp
@@ -0,0 +1,6 @@
+/* crear una matriz y hacer picas y fijas 
+
+que no sean numeros superiores a 9 e inferiores a 0 
+cuando se llene la matriz el primer intento se pierde
+matriz de 10 filas y 6 columnas
+*/
